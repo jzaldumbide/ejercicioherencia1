29c4da60f6e723ad5446456079a2cba887d80d17
Juan Zaldumbide <jzaldumbide@gmail.com>
2023-12-05 07:51:31 -0500

    Commit 2

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,16 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
+    Empleado E1=new Empleado("Pedro","0001");
+    Operario O1=new Operario("Juan","0002");
+    Directivo D1=new Directivo("Andrea","0003");
 
+
+        O1.saludoOperario();
+        O1.saludo();
+
+        D1.saludoDirectivo();
+        D1.saludo();
 
     }
 }
